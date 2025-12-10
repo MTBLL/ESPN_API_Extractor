b95ef5d5ba1c06d9b205fee2e7a7e3e388f43593
Taylor "TP" Pubins <tpubz@icloud.com>
2025-12-09 17:21:21 -0700

    add core requests to exports

--- espn_api_extractor/requests/__init__.py
+++ espn_api_extractor/requests/__init__.py
@@ -1,9 +1,11 @@
 __all__ = [
+    "EspnCoreRequests",
     "EspnFantasyRequests",
     "ESPNAccessDenied",
     "ESPNInvalidLeague",
     "ESPNUnknownError",
 ]
 
+from .core_requests import EspnCoreRequests
 from .exceptions import ESPNAccessDenied, ESPNInvalidLeague, ESPNUnknownError
 from .fantasy_requests import EspnFantasyRequests
