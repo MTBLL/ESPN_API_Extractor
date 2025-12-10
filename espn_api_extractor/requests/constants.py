866b2c3549085c21670c49032b8ae9dee095e1a5
Taylor "TP" Pubins <tpubz@icloud.com>
2025-12-09 17:05:49 -0700

    modify sports abbrv to be enum

--- espn_api_extractor/requests/constants.py
+++ espn_api_extractor/requests/constants.py
@@ -1,12 +1,17 @@
+from enum import Enum
+
 FANTASY_BASE_ENDPOINT = "https://lm-api-reads.fantasy.espn.com/apis/v3/games/"
 NEWS_BASE_ENDPOINT = "https://site.api.espn.com/apis/fantasy/v3/games/"
-FANTASY_SPORTS = {
-    "nfl": "ffl",
-    "nba": "fba",
-    "nhl": "fhl",
-    "mlb": "flb",
-    "wnba": "wfba",
-}
+
+
+class FantasySports(Enum):
+    NFL = "ffl"
+    NBA = "fba"
+    NHL = "fhl"
+    MLB = "flb"
+    WNBA = "wfba"
+
+
 ESPN_CORE_ENDPOINT = "http://sports.core.api.espn.com/v2"
 ESPN_CORE_MLB_ENDPOINT = ESPN_CORE_ENDPOINT + "/sports/baseball/leagues/mlb"
 ESPN_CORE_MLB_PLAYERS_ENDPOINT = ESPN_CORE_MLB_ENDPOINT + "/athletes"
